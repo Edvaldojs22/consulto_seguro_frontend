1052b7bcc7cfbb30cf9c2a9484ffc794122c0302
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2024-01-02 18:35:11 -0300

    att

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/consulto_seguro_frontend",
   plugins: [react()],
   base: "/consulto_seguro_frontend",
 })
